f25c246a30ba0e7a00837cb4488970ddbb9d327f
Shawn Stewart <s.stewart0220@gmail.com>
2019-01-08 15:45:14 -0500

    Basic nav layout

--- PoweredLabs/frontend/src/Components/Nav/Nav.js
+++ PoweredLabs/frontend/src/Components/Nav/Nav.js
@@ -5,8 +5,56 @@ import "./Nav.css";
 
 export default function Nav() {
   return (
-    <div>
+    <div className="nav">
       <NavHeader />
+      <div className="container nav-main">
+        <a
+          href="http://localhost:3000"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="nav-main-logo">Logo</div>
+        </a>
+
+        <ul className="nav-main-menu">
+          <li>
+            <a
+              href="http://localhost:3000"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Services
+            </a>
+          </li>
+          <li>
+            <a
+              href="http://localhost:3000"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Portfolio
+            </a>
+          </li>
+          <li>
+            <a
+              href="http://localhost:3000"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Blogs
+            </a>
+          </li>
+          <li>
+            <a
+              href="http://localhost:3000"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
